ccb975f560416ca3ad8f24cf265fd7ca1ecb231a
mujambajecha <45811027+mujambajecha@users.noreply.github.com>
2022-07-10 19:31:25 -0400

    Update pgclone.sh

--- functions/pgclone.sh
+++ functions/pgclone.sh
@@ -92,7 +92,7 @@ tee <<-EOF
 Quitting? Type > exit
 NOTE: Copy & Paste Url into Browser | Use Correct Google Account!
 
-https://accounts.google.com/o/oauth2/auth?client_id=$public&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=https://www.googleapis.com/auth/drive&response_type=code
+https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&response_type=code&state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2.example.com%2Ftoken&redirect_uri=com.example.app%3A/oauth2redirect&client_id=client_id
 
 EOF
   read -p '↘️  Token | PRESS [ENTER]: ' token < /dev/tty
@@ -185,7 +185,7 @@ tee <<-EOF
 Quitting? Type > exit
 NOTE: Copy & Paste Url into Browser | Use Correct Google Account!
 
-https://accounts.google.com/o/oauth2/auth?client_id=$public&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=https://www.googleapis.com/auth/drive&response_type=code
+https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&response_type=code&state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2.example.com%2Ftoken&redirect_uri=com.example.app%3A/oauth2redirect&client_id=client_id
 
 EOF
   read -p '↘️  Token | PRESS [ENTER]: ' token < /dev/tty
